372c33b1377a591b284b9677749bfee8d2b85570
ChangLiuuu <spiritchang@hotmail.com>
2017-08-20 17:33:55 -0700

    mongoose

--- api/data/db.js
+++ api/data/db.js
@@ -4,4 +4,13 @@
 var mongoose = require('mongoose');
 var dburl = 'mongodb://localhost:27017/mean2';
 
-mongoose.connect(dburl);
+mongoose.connect(dburl, {useMongoClient: true});
+mongoose.connection.on('connected', function() {
+    console.log('Mongoose connected to' + dburl);
+});
+mongoose.connection.on('disconnected', function() {
+    console.log('Mongoose disconnected');
+});
+mongoose.connection.on('error', function(err) {
+    console.log('Mongoose connection error:' + err);
+});
