2f20c861a5f789207c0dd8017f78bfd449ec4411
ChangLiuuu <spiritchang@hotmail.com>
2017-08-21 23:59:21 -0700

    REST

--- api/data/db.js
+++ api/data/db.js
@@ -4,13 +4,15 @@
 var mongoose = require('mongoose');
 var dburl = 'mongodb://localhost:27017/mean2';
 
-mongoose.connect(dburl, {useMongoClient: true});
+mongoose.connect(dburl, {useMongoClient: true});//{xxx} makes warning disappeared.
 mongoose.connection.on('connected', function() {
     console.log('Mongoose connected to' + dburl);
 });
+
 mongoose.connection.on('disconnected', function() {
     console.log('Mongoose disconnected');
 });
+
 mongoose.connection.on('error', function(err) {
     console.log('Mongoose connection error:' + err);
 });
