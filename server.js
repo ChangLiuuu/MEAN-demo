93c215d9cd65ad65da3483ca593dcbbc954f8d7a
ChangLiuuu <spiritchang@hotmail.com>
2017-09-28 18:02:09 -0700

    modify port

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ let bodyParser = require('body-parser');
 let routes = require('./api/routes/index');
 
 let app = express();
+
+let port = process.env.PORT;
+
 app.set('port', 3000);
 
 app.use(function(req, res, next) {
@@ -17,14 +20,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.use('/fonts', express.static(__dirname + '/fonts'));
 
-
 // enable parsing of posted forms
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
 
-var server = app.listen(app.get('port'), (req, res) => {
-    var port = server.address().port;
-    console.log('Here is port ' + port);
+app.listen(port, function(){
+    console.log(` listening on port ${port}`);
 });
+
